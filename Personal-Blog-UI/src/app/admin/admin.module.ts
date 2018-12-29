21d8032918cf6c6e98162b7d68b1bacf105744be
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-12-30 02:38:45 +0600

    admin module is updated

---
+++ Personal-Blog-UI/src/app/admin/admin.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    EditPostComponent,
+    CreatePostComponent,
+    AboutComponent
+  ]
+})
+export class AdminModule { }
