449a4aa4524643e8ff7267873898620382ad6af4
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-12-31 21:18:53 +0600

    some component is imported

--- Personal-Blog-UI/src/app/app.module.ts
+++ Personal-Blog-UI/src/app/app.module.ts
@@ -1,23 +1,34 @@
+import { AppRouteModule } from './app-route.module';
+import { AdminRouteModule } from './admin/admin-route.module';
+import { UserRouteModule } from './user/user-route.module';
 import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     UserModule,
     AdminModule,
+    AppRouteModule,
+    UserRouteModule,
+    AdminRouteModule,
+    MatCardModule,
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
