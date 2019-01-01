df7f5cdc5c0429dbf8ac3cba6ac341e6a664a7f6
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-01 18:53:21 +0600

    reading post is imported

--- Personal-Blog-UI/src/app/user/user.module.ts
+++ Personal-Blog-UI/src/app/user/user.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CommentComponent } from './comment/comment.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ReadingPostComponent } from './reading-post/reading-post.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   declarations: [
     LoginComponent,
     RegisterComponent,
     CommentComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    ReadingPostComponent
   ]
 })
 export class UserModule { }
