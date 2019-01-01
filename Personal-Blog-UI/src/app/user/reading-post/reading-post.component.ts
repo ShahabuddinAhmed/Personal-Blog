6c8997f70d0726dc22ae1204ce31b7e014751c11
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-01 18:54:04 +0600

    reading post is created

---
+++ Personal-Blog-UI/src/app/user/reading-post/reading-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-post',
+  templateUrl: './reading-post.component.html',
+  styleUrls: ['./reading-post.component.scss']
+})
+export class ReadingPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
