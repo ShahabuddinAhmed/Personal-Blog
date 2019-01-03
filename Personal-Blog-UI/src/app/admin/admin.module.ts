d6209830f47ec2b2c9d00e50cffb09f8cb4c4af3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-03 21:01:37 +0600

    admin module is updated

--- Personal-Blog-UI/src/app/admin/admin.module.ts
+++ Personal-Blog-UI/src/app/admin/admin.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
+import { UserModule } from './../user/user.module';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { EditPostComponent } from './edit-post/edit-post.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { AboutComponent } from './about/about.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule,
+    MDBBootstrapModule.forRoot()
   ],
-  exports: [
-    CreatePostComponent
-  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   declarations: [
     LoginComponent,
     RegisterComponent,
