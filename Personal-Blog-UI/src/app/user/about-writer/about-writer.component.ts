d6e67576f6981b22ff9a3e8da9695b19db9cc10e
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-03 21:06:42 +0600

    about writer component is created

---
+++ Personal-Blog-UI/src/app/user/about-writer/about-writer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-writer',
+  templateUrl: './about-writer.component.html',
+  styleUrls: ['./about-writer.component.scss']
+})
+export class AboutWriterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
