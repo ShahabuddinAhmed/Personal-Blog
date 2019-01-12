35552b9c1a5a0ca5404b5e21d315e7294ff29bb5
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-12 21:31:11 +0600

    user register business logic is updated

--- Personal-Blog-UI/src/app/user/register/register.component.ts
+++ Personal-Blog-UI/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Register } from '../reading-post/register';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  hide = true;
+  hide1 = true;
+  public Register: FormGroup;
+  public name: FormControl;
+  public email: FormControl;
+  public password: FormControl;
+  public confirmPassword: FormControl;
+  register: Register;
+
+  private createFormGroup(): void {
+    this.Register = new  FormGroup( {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    });
+  }
+
+  private createFormControls(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(6),
+      Validators.maxLength(60)
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30)
+    ]);
+    this.confirmPassword = new FormControl('', [
+      Validators.required
+    ]);
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createFormGroup();
+  }
+
+  onSubmit() {
+    this.register = new Register();
+    this.register.userName = this.name.value;
+    this.register.userEmail = this.email.value;
+    this.register.userPassword = this.password.value;
+    this.register.userConfirmPassword = this.confirmPassword.value;
   }
 
 }
