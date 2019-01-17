51cfd35788e0c7af16fd26b2a15ba669ea8c60e3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-18 03:22:45 +0600

    admin router module is updated

--- Personal-Blog-UI/src/app/admin/admin-route.module.ts
+++ Personal-Blog-UI/src/app/admin/admin-route.module.ts
@@ -6,13 +6,25 @@ import { RegisterComponent } from '../admin/register/register.component';
 import { AboutComponent } from '../admin/about/about.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { EditPostComponent } from './edit-post/edit-post.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { PostCatagoryComponent } from './post-catagory/post-catagory.component';
+import { PublishedPostComponent } from './published-post/published-post.component';
+import { PersonalPostComponent } from './personal-post/personal-post.component';
+import { DraftPostComponent } from './draft-post/draft-post.component';
 
 const routes: Routes = [
   { path: 'admin-login', component: LoginComponent },
   { path: 'admin-register', component: RegisterComponent },
   { path: 'createpost', component: CreatePostComponent },
   { path: 'editpost', component: EditPostComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'admin', component: DashboardComponent },
+  { path: 'postcatagory', component: PostCatagoryComponent },
+  { path: 'adminpanel', component: AdminPanelComponent },
+  { path: 'published', component: PublishedPostComponent },
+  { path: 'personal', component: PersonalPostComponent },
+  { path: 'draft', component: DraftPostComponent }
 ];
 
 @NgModule({
