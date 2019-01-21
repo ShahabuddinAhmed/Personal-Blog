8b0ce8d14f09a20251db2da589069911b28fc466
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-21 11:07:03 +0600

    admin draft-post is created

---
+++ Personal-Blog-UI/src/app/admin/draft-post/draft-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-draft-post',
+  templateUrl: './draft-post.component.html',
+  styleUrls: ['./draft-post.component.scss']
+})
+export class DraftPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
