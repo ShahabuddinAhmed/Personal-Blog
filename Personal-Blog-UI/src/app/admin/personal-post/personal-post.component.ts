f532c90af08f16360f29b940434d5381a95e9017
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-22 19:49:00 +0600

    admin personal component is created

---
+++ Personal-Blog-UI/src/app/admin/personal-post/personal-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-post',
+  templateUrl: './personal-post.component.html',
+  styleUrls: ['./personal-post.component.scss']
+})
+export class PersonalPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
