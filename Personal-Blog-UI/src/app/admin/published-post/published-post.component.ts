88050586729c40b6cd7f72e109bff4ec43bca361
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-22 19:50:06 +0600

    admin publish-post component is created

---
+++ Personal-Blog-UI/src/app/admin/published-post/published-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-published-post',
+  templateUrl: './published-post.component.html',
+  styleUrls: ['./published-post.component.scss']
+})
+export class PublishedPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
