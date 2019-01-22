d8158ab55b638c9950add09f862a1c009ba3b7a6
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-22 19:49:31 +0600

    admin post-category component is created

---
+++ Personal-Blog-UI/src/app/admin/post-catagory/post-catagory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-catagory',
+  templateUrl: './post-catagory.component.html',
+  styleUrls: ['./post-catagory.component.scss']
+})
+export class PostCatagoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
