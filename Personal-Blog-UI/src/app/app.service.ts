a3003159f8869e19d3098c6f5af09e73efe10712
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-23 22:12:50 +0600

    app service is created

---
+++ Personal-Blog-UI/src/app/app.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Post } from './admin/models/post';
+import { Catagory } from './admin/models/catagory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  jwtHelper = new JwtHelperService();
+
+  getAllPost(): Observable<Post[]> {
+    return this.http.get<Post[]>('http://localhost:3000/post/all');
+  }
+
+  getCatagoryName(): Observable<Catagory[]> {
+    return this.http.get<Catagory[]>('http://localhost:3000/catagory/all');
+  }
+
+}
