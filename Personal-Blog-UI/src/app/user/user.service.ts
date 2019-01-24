9f2568ef54d8f0d773d43f901cdd51cea382c43b
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-24 11:03:16 +0600

    user service is modified

--- Personal-Blog-UI/src/app/user/user.service.ts
+++ Personal-Blog-UI/src/app/user/user.service.ts
@@ -1,9 +1,42 @@
+import { Register } from './models/register';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Login } from './models/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  jwtHelper = new JwtHelperService();
+
+  addNewUser(_register: Register) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post('http://localhost:3000/user/register', _register, httpOptions);
+  }
+
+  reqLogin(_login: Login) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post('http://localhost:3000/user/login', _login, httpOptions);
+  }
+
+  setToken(auth: any) {
+    localStorage.setItem('token', auth.token);
+  }
 }
