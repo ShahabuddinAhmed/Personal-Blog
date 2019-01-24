8953fd88329856f52d5480e378fcbb5b3655eac6
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-24 11:13:13 +0600

    home business logic is added

--- Personal-Blog-UI/src/app/home/home.component.ts
+++ Personal-Blog-UI/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../admin/models/post';
+import { AppService } from '../app.service';
+import { Catagory } from '../admin/models/catagory';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public allPost: Post[];
+  public catagoryName: Catagory[];
+
+  constructor(private activeRoute: ActivatedRoute, private _appService: AppService, private router: Router) { }
 
   ngOnInit() {
+    this.getPost();
+    this.getCatagory();
+  }
+
+  private getPost() {
+    this._appService.getAllPost().subscribe(data => {
+      this.allPost = data;
+      console.log(this.allPost);
+    },
+    err => {
+      console.log(err);
+    }
+      );
+  }
+
+  private getCatagory() {
+    this._appService.getCatagoryName().subscribe(data => {
+      this.catagoryName = data;
+      console.log(this.catagoryName);
+    },
+    err => {
+      console.log(err);
+    }
+      );
   }
 
 }
