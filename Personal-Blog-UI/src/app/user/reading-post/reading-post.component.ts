64272bc7ae3b43aaa0b5c8a3ef35ebc6079d2257
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-25 18:47:37 +0600

    user business logic is updated

--- Personal-Blog-UI/src/app/user/reading-post/reading-post.component.ts
+++ Personal-Blog-UI/src/app/user/reading-post/reading-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Post } from '../../admin/models/post';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-reading-post',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadingPostComponent implements OnInit {
 
-  constructor() { }
+  public PostID: string;
+  public readingPost: Post;
+
+  constructor(private _userService: UserService, private router: Router, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getPostID();
+    this.getOnePost(this.PostID);
+  }
+
+  private getPostID() {
+    this.activeRoute.params.subscribe(param => {
+      this.PostID = param['id'];
+      console.log(this.PostID);
+    });
+  }
+
+  private getOnePost(ID: string) {
+    this._userService._getOnePost(ID)
+    .subscribe(data => {
+      this.readingPost = data;
+    },
+    err => {
+      console.log(err);
+    });
   }
 
 }
