62ea909e7e6587254c9911abdf4c4c3317381b47
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-27 00:29:52 +0600

    about writer business logic is updated

--- Personal-Blog-UI/src/app/user/about-writer/about-writer.component.ts
+++ Personal-Blog-UI/src/app/user/about-writer/about-writer.component.ts
@@ -1,4 +1,8 @@
+import { Post } from './../models/post';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { Catagory } from '../models/catagory';
 
 @Component({
   selector: 'app-about-writer',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutWriterComponent implements OnInit {
 
-  constructor() { }
+  public allPost: Post[];
+  public catagoryName: Catagory[];
+
+  constructor(private _userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.getPost();
+    this.getCatagory();
+  }
+
+  private getPost() {
+    this._userService.getAllPost().subscribe(data => {
+      this.allPost = data;
+      console.log(this.allPost);
+    },
+    err => {
+      console.log(err);
+    }
+      );
+  }
+
+  private getCatagory() {
+    this._userService.getCatagoryName().subscribe(data => {
+      this.catagoryName = data;
+      console.log(this.catagoryName);
+    },
+    err => {
+      console.log(err);
+    }
+      );
   }
 
 }
