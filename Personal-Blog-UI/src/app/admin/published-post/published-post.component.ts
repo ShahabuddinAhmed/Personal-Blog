a1e75d3bebf40c0a8c473e98221ba9557e9e9cf9
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-27 00:59:03 +0600

    admin published post is updated

--- Personal-Blog-UI/src/app/admin/published-post/published-post.component.ts
+++ Personal-Blog-UI/src/app/admin/published-post/published-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-published-post',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublishedPostComponent implements OnInit {
 
-  constructor() { }
+  public postType: string;
+  public publishedPostList: Post[];
+  public post: Post;
+
+  constructor(private _adminService: AdminService, private router: Router) { }
 
   ngOnInit() {
+    this.getPostType();
+  }
+
+  private getPostType() {
+    this.postType = 'Public';
+    this._adminService._getPostType(this.postType)
+    .subscribe(data => {
+      this.publishedPostList = data;
+      console.log(this.publishedPostList);
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  makeDraft(ID: string) {
+    this.post = new Post;
+    this.post.postType = 'Draft';
+    this._adminService._changePostType(ID, this.post)
+    .subscribe(data => {
+      console.log(data);
+      this.getPostType();
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  makePersonal(ID: string) {
+    this.post = new Post;
+    this.post.postType = 'Personal';
+    this._adminService._changePostType(ID, this.post)
+    .subscribe(data => {
+      console.log(data);
+      this.getPostType();
+    },
+    err => {
+      console.log(err);
+    });
   }
 
 }
