bbccd1cd90531f459330acd7dcf58710f088cbdc
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-27 02:29:03 +0600

    admin personal post is updated

--- Personal-Blog-UI/src/app/admin/personal-post/personal-post.component.ts
+++ Personal-Blog-UI/src/app/admin/personal-post/personal-post.component.ts
@@ -12,6 +12,7 @@ export class PersonalPostComponent implements OnInit {
 
   public postType: string;
   public personalPostList: Post[];
+  public post: Post;
 
   constructor(private _adminService: AdminService, private router: Router) { }
 
@@ -25,6 +26,32 @@ export class PersonalPostComponent implements OnInit {
     .subscribe(data => {
       this.personalPostList = data;
       console.log(this.personalPostList);
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  makeDraft(ID: string) {
+    this.post = new Post;
+    this.post.postType = 'Draft';
+    this._adminService._changePostType(ID, this.post)
+    .subscribe(data => {
+      console.log(data);
+      this.getPostType();
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  makePublic(ID: string) {
+    this.post = new Post;
+    this.post.postType = 'Public';
+    this._adminService._changePostType(ID, this.post)
+    .subscribe(data => {
+      console.log(data);
+      this.getPostType();
     },
     err => {
       console.log(err);
