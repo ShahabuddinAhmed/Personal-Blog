aa2a96de2e325e6613de895b074f3bec04f6cd24
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-30 11:26:28 +0600

    admin module updated

--- Personal-Blog-UI/src/app/admin/admin.module.ts
+++ Personal-Blog-UI/src/app/admin/admin.module.ts
@@ -7,11 +7,54 @@ import { EditPostComponent } from './edit-post/edit-post.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { AboutComponent } from './about/about.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatRippleModule,
+  MatTooltipModule,
+  MatTabsModule
+} from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { PostCatagoryComponent } from './post-catagory/post-catagory.component';
+import { PublishedPostComponent } from './published-post/published-post.component';
+import { PersonalPostComponent } from './personal-post/personal-post.component';
+import { DraftPostComponent } from './draft-post/draft-post.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
@@ -20,7 +63,14 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
     RegisterComponent,
     EditPostComponent,
     CreatePostComponent,
-    AboutComponent
+    AboutComponent,
+    DashboardComponent,
+    SidebarComponent,
+    AdminPanelComponent,
+    PostCatagoryComponent,
+    PublishedPostComponent,
+    PersonalPostComponent,
+    DraftPostComponent
   ]
 })
 export class AdminModule { }
