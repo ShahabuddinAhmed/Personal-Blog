177f2c97f874eb608673f0b6eb9f6660586ac6a4
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-30 11:28:30 +0600

    admin login business logic is updated

--- Personal-Blog-UI/src/app/admin/login/login.component.ts
+++ Personal-Blog-UI/src/app/admin/login/login.component.ts
@@ -1,4 +1,8 @@
+import { Login } from './../models/login';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
+  public loginForm: FormGroup;
+  public email: FormControl;
+  public password: FormControl;
+  login: Login;
 
-  constructor() { }
+  private createForm(): void {
+    this.loginForm = new FormGroup( {
+      email: this.email,
+      password: this.password
+    });
+  }
+  private CreateFormControls(): void {
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(60),
+      Validators.email
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(100)
+    ]);
+  }
+
+  constructor(private _adminService: AdminService, private router: Router) { }
 
   ngOnInit() {
+    this.CreateFormControls();
+    this.createForm();
+  }
+
+  onSubmit() {
+    this.login = new Login();
+    this.login.adminEmail = this.email.value;
+    this.login.adminPassword = this.password.value;
+    this._adminService.reqLogin(this.login)
+    .subscribe(data => {
+      console.log(data);
+      this._adminService.setToken(data);
+      this.router.navigate(['/']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
