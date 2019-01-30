65874d67a611544c2a46fdf95c90bc76f9974fc7
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-30 11:23:40 +0600

    app routing module is updated

--- Personal-Blog-UI/src/app/app-route.module.ts
+++ Personal-Blog-UI/src/app/app-route.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent }
 ];
