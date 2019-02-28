e6cafb9c37c2b92e9a162471f503bafa219591ff
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-28 16:42:51 +0600

    admin editdraft post is created

---
+++ Personal-Blog-UI/src/app/admin/editdraftpost/editdraftpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editdraftpost',
+  templateUrl: './editdraftpost.component.html',
+  styleUrls: ['./editdraftpost.component.scss']
+})
+export class EditdraftpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
