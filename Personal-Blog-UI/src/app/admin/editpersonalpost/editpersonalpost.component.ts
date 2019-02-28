0e8a1a56ab5b8167e91ed4444f30b3896e18a581
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-28 16:43:41 +0600

    admin editd-personalpost post is created

---
+++ Personal-Blog-UI/src/app/admin/editpersonalpost/editpersonalpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editpersonalpost',
+  templateUrl: './editpersonalpost.component.html',
+  styleUrls: ['./editpersonalpost.component.scss']
+})
+export class EditpersonalpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
