56e972ee128bc0bc0591bc3eaadd1d2a7e782630
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-03 11:01:36 +0600

    sidebar business logic is added

--- Personal-Blog-UI/src/app/admin/sidebar/sidebar.component.ts
+++ Personal-Blog-UI/src/app/admin/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public draftNum: number;
+  public publicNum: number;
+  public personalNum: number;
+
+  constructor(private _adminService: AdminService, private router: Router) { }
 
   ngOnInit() {
+    this.getDraftNum('Draft');
+    this.getPublicNum('Public');
+    this.getPersonalNum('Personal');
+  }
+
+  private getDraftNum(Name: string) {
+    this._adminService._getPostType(Name)
+    .subscribe(data => {
+      console.log(data);
+      this.draftNum = data.length;
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  private getPublicNum(Name: string) {
+    this._adminService._getPostType(Name)
+    .subscribe(data => {
+      console.log(data);
+      this.publicNum = data.length;
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  private getPersonalNum(Name: string) {
+    this._adminService._getPostType(Name)
+    .subscribe(data => {
+      console.log(data);
+      this.personalNum = data.length;
+    },
+    err => {
+      console.log(err);
+    });
   }
 
 }
