f3e2e750e74d9ebe6868d10163d05f8992f880b9
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-14 19:52:56 +0600

    admin publsihed post bisiness logic is added

--- Personal-Blog-UI/src/app/admin/published-post/published-post.component.ts
+++ Personal-Blog-UI/src/app/admin/published-post/published-post.component.ts
@@ -13,6 +13,8 @@ export class PublishedPostComponent implements OnInit {
   public postType: string;
   public publishedPostList: Post[];
   public post: Post;
+  public customFile: File = null;
+  public userID: string;
 
   constructor(private _adminService: AdminService, private router: Router) { }
 
@@ -56,6 +58,51 @@ export class PublishedPostComponent implements OnInit {
     err => {
       console.log(err);
     });
+  }
+
+  deletePublished(ID: string) {
+    this._adminService._deletePost(ID)
+    .subscribe(data => {
+      console.log(data);
+      this.getPostType();
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.customFile = fileInput.target.files[0];
+    // this.product.photo = fileInput.target.files[0]['name'];
+    console.log(this.customFile);
+  }
+
+  // upload() {
+  //   const userID = '5c49cbb4a2cb7302e022d4cc';
+  //   const fd = new FormData();
+  //     fd.append('picture', this.customFile, this.customFile.name);
+  //     fd.append('userId', userID);
+  //     this._adminService._uploadCoverImage(this.customFile, userID).subscribe(data => {
+  //       // console.log(data["message"]);
+  //      console.log(data);
+  //     }, err => {
+  //       console.log(err);
+  //     });
+  // }
+
+  upload(postID: string) {
+    // this.userID = '5c49cbb4a2cb7302e022d4cc';
+    this._adminService.updateProfileImage(this.customFile, postID).subscribe(data => {
+      // console.log(data["message"]);
+     console.log(data);
+     this.getPostType();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  editPublishedPost(id: string) {
+    this.router.navigate([`editpublishedpost/${id}`]);
   }
 
 }
