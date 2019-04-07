4c8f95d05c74fd97a62affce8d25b96f5fe2896a
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-04-07 21:19:55 +0600

    auth is added

---
+++ Personal-Blog-UI/src/app/auth/common.service.ts
@@ -0,0 +1,44 @@
+import { AuthService } from './auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  jwtHelper = new JwtHelperService();
+
+  constructor(public auth: AuthService) { }
+
+  isAuthenticated() {
+    if (!this.auth.isAuthenticated()) {
+      return false;
+    }
+    return true;
+  }
+
+  isUser() {
+    const token = localStorage.getItem('token');
+
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+
+    if (!this.auth.isAuthenticated() || tokenPayload.userRoll !== 'User') {
+      return false;
+    }
+
+    return true;
+  }
+
+  isAdmin() {
+    const token = localStorage.getItem('token');
+
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+
+    if (!this.auth.isAuthenticated()) {
+      return false;
+    }
+
+    return true;
+  }
+}
